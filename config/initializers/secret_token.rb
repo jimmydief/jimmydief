b5c32d6fd723697c8efea9f53ba5411dc68f8b65
James Diefenderfer <jimmy@launchpadtoys.com>
2013-05-22 01:49:29 -0700

    INITIAL - created new rails app
    Signed-off-by: James Diefenderfer <jimmy@launchpadtoys.com>

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Jimmydief::Application.config.secret_token = 'f8845736181a0863e823ce4b069b74cbef6c5051a609173116421c3c64c571db090c133f2f4f5016434ab8d2aadef2fd9bedf95c38266c6d56ae5e251232a4de'
