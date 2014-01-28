197515ea4a5419dd3b9491e0af0aabda1c65a205
James Diefenderfer <jimmydief@gmail.com>
2014-01-28 13:08:02 -0800

    CONFIG - turn off initialize on precompile
    Signed-off-by: James Diefenderfer <jimmydief@gmail.com>

--- config/application.rb
+++ config/application.rb
@@ -52,6 +52,9 @@ module Jimmydief
     # in your app. As such, your models will need to explicitly whitelist or blacklist accessible
     # parameters by using an attr_accessible or attr_protected declaration.
     config.active_record.whitelist_attributes = true
+    
+    # to speed up asset precompiles, it’s recommended that you tell Rails to only partially load your app
+    config.assets.initialize_on_precompile = false
 
     # Enable the asset pipeline
     config.assets.enabled = true
