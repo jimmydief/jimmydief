ff085962e834e908b04d99c741a7af53a5bfa8eb
James Diefenderfer <jimmydief@gmail.com>
2014-07-10 01:24:38 -0400

    move docready JS back into HEAD

--- js/main.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function () {
-  $("ul.nav li a:contains({{ page.title }})").parent("li").addClass('active');
-  if ($("ul.nav li.active").length === 0) {
-    $("ul.nav li:first-child").addClass('active');
-  }
-
-  $("button").tooltip({});
-});
